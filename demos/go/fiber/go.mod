1d4934c42d2fa9e55dea6ade6f0b733c1a627b50
Gagan Bhatia <gagan3012@users.noreply.github.com>
2020-10-25 17:54:50 +0300

    Go fiber framework

---
+++ demos/go/fiber/go.mod
@@ -0,0 +1,17 @@
+module go-fiber
+
+go 1.15
+
+require (
+	github.com/LoginRadius/go-sdk v0.0.0-20190613234105-3e489fe94bc8
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/gofiber/adaptor v0.2.0
+	github.com/gofiber/adaptor/v2 v2.0.1
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/fiber/v2 v2.1.1
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/klauspost/compress v1.11.1 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	golang.org/x/sys v0.0.0-20201024232916-9f70ab9862d5 // indirect
+)
---
+++ demos/go/fiber/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	lr "github.com/LoginRadius/go-sdk"
+	lrauthentication "github.com/LoginRadius/go-sdk/api/authentication"
+	"github.com/LoginRadius/go-sdk/lrerror"
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+type Output struct {
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+	Status  string      `json:"status"`
+}
+
+func main() {
+
+	app := fiber.New()
+	//env files
+	err := godotenv.Load("config.env")
+
+	if err != nil {
+		log.Fatal("Error loading env files")
+	}
+
+	app.Static("/", "../../../theme")
+
+	app.Get("/api/profile", adaptor.HTTPHandler(handler(handleget)))   //Get request
+	app.Post("/api/profile", adaptor.HTTPHandler(handler(handlepost))) //Post Request
+
+	app.Listen(":3000")
+}
+
+func handler(f http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(f)
+}
+
+func handleget(w http.ResponseWriter, r *http.Request) {
+	var errors string
+	var out Output
+	out.Message = "an error occoured"
+	out.Status = "error"
+
+	token := r.URL.Query().Get("token")
+	respCode := 200
+
+	cfg := lr.Config{
+		ApiKey:    os.Getenv("APIKEY"),
+		ApiSecret: os.Getenv("APISECRET"),
+	}
+	lrclient, err := lr.NewLoginradius(
+		&cfg,
+		map[string]string{"token": token},
+	)
+	if err != nil {
+		errors = errors + err.(lrerror.Error).OrigErr().Error()
+		respCode = 500
+	}
+
+	res, err := lrauthentication.Loginradius(lrauthentication.Loginradius{lrclient}).GetAuthReadProfilesByToken()
+	if err != nil {
+		errors = errors + err.(lrerror.Error).OrigErr().Error()
+		respCode = 500
+	}
+
+	var data map[string]interface{}
+	er := json.Unmarshal([]byte(res.Body), &data)
+	if er != nil {
+		//panic(err)
+	}
+	if data["Uid"] != nil {
+		out.Data = data
+		out.Message = "Profile fetched"
+		out.Status = "success"
+	} else {
+		out.Message = "Account does not exist."
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(respCode)
+	if errors != "" {
+		log.Printf(errors)
+		w.Write([]byte(errors))
+	}
+	b, e := json.Marshal(out)
+	if e != nil {
+		//panic(err)
+	}
+	w.Write([]byte(b))
+}
+
+func handlepost(w http.ResponseWriter, r *http.Request) {
+	var errors string
+	respCode := 200
+	var out Output
+	out.Message = "an error occoured"
+	out.Status = "error"
+
+	cfg := lr.Config{
+		ApiKey:    os.Getenv("APIKEY"),
+		ApiSecret: os.Getenv("APISECRET"),
+	}
+	token := r.URL.Query().Get("token")
+
+	lrclient, err := lr.NewLoginradius(
+		&cfg,
+		map[string]string{"token": token},
+	)
+	if err != nil {
+		errors = errors + err.(lrerror.Error).OrigErr().Error()
+		respCode = 500
+	}
+
+	data := struct {
+		FirstName string
+		LastName  string
+		About     string
+	}{}
+
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &data)
+
+	response, err := lrauthentication.Loginradius(lrauthentication.Loginradius{lrclient}).PutAuthUpdateProfileByToken(
+		data,
+	)
+
+	if err != nil {
+		errors = errors + err.(lrerror.Error).OrigErr().Error()
+		respCode = 500
+	} else {
+		var data map[string]interface{}
+		er := json.Unmarshal([]byte(response.Body), &data)
+		if er != nil {
+			//panic(err)
+		}
+		if data["IsPosted"] == true {
+			out.Message = "Profile has been updated successfully."
+			out.Status = "success"
+			out.Data = data["Data"]
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(respCode)
+	if errors != "" {
+		log.Printf(errors)
+		w.Write([]byte(errors))
+		return
+	}
+	b, e := json.Marshal(out)
+	if e != nil {
+		//panic(err)
+	}
+	w.Write([]byte(b))
+}
